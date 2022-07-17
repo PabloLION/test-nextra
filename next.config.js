d9bf0223598e01e3eedf70cb413e377f4e33506f
Pablo LION <36828324+Pablion@users.noreply.github.com>
2022-07-17 21:44:26 +0200

    chore: setup nextra

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// };
+
+// module.exports = nextConfig;
+
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.js",
+  // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
   reactStrictMode: true,
   swcMinify: true,
-}
-
-module.exports = nextConfig
+});
+module.exports = withNextra();
